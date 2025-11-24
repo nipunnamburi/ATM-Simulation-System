c09619d8df6ce437027c34fe562dd14609014923
nipunnamburi <nipunnamburi@gmail.com>
2025-11-24 19:17:02 +0530

    Add files via upload

---
+++ src/main/java/com/atm/atmproject/AtmprojectApplication.java
@@ -0,0 +1,13 @@
+package com.atm.atmproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atm/atmproject/AtmprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atm.atmproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
